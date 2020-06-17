bb2b07875ddb503c5e31e37c030588e10d6cd33c
nilayingle <nilayingle18@gmail.com>
2020-06-17 17:34:52 +0530

    Application Development Done....

---
+++ README.md
@@ -0,0 +1,14 @@
+* This app is developed using flutter framework.
+* This app can run on Android and iOS both.
+
+How to run this app:
+* First download flutter sdk from flutter official websit.
+* Download Android Studio from official website.
+* Install Flutter and Dart plugin in Android Studio.
+* Click on Pub get to download necessary dependancies.
+* Also download necessary android studio dependencies.
+* To run this app there are two ways - 
+  1] Download Android emulator in android studio and run on it.
+  2] Use your Android/iOS device by connecting your device to PC using USB cable.
+
+* This application is developed by Nilay Ingle.
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 60.0,
+                backgroundImage: AssetImage('images/nilay.jpg'),
+              ),
+              Text(
+                'Nilay Ingle',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.green[500],
+                  ),
+                  title: Text(
+                    '+91 7776962744',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.red[500],
+                  ),
+                  title: Text(
+                    'nilayingle18@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.teal.shade900,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
